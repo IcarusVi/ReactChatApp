172fea4d98f551a99c804fb6df8a940d0a245f69
IcarusVi <zscyrus31@gmail.com>
2021-03-07 19:18:25 -0500

    Removed unneeded line

--- chatclient/src/App.js
+++ chatclient/src/App.js
@@ -2,15 +2,13 @@ import './App.css';
 import Container from '@material-ui/core/Container';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { makeStyles } from "@material-ui/core/styles";
 import Messages from './components/messagesContainer'
-import SendText from './components/sendText';
+
 /*
 Simple layout
 Header with room name
 Middle will be messages
 Last a textfield to enter text and send button
-
 */
 
 
