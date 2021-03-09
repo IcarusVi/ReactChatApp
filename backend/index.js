3fc1e915d4d9b8a7f895cffc80489c526fd6efdb
IcarusVi <zscyrus31@gmail.com>
2021-03-08 23:05:24 -0500

    Implemented message updates

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8000;
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    credentials: true
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log(`${socket.id} connected`)
+  socket.on('message sent', (msg) => {
+    console.log(msg)
+    io.emit('message received', msg)
+  })
+})
+
+http.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+
--- chatclient/src/components/messagesContainer.js
+++ chatclient/src/components/messagesContainer.js
@@ -1,9 +1,14 @@
 import { makeStyles } from '@material-ui/core/styles';
+import {io} from "socket.io-client";
 import Paper from '@material-ui/core/Paper';
 import fakeMessages from '../fakeMessages.json'
 import SendText from './sendText'
-import {useState} from 'react'
-import Message from './message'
+import {useState, useEffect, useRef} from 'react'
+import Message from './message';
+
+const endPoint = 'http://localhost:8000'
+let socket;
+
 
 const useStyles = makeStyles({
     messageContainer: {
@@ -13,11 +18,24 @@ const useStyles = makeStyles({
 
 const Messages = () => {
     const classes = useStyles();
-    const [texts, setTexts] = useState(fakeMessages)
+    const socketRef = useRef();
+    
+    const [texts, setTexts] = useState([])
+
+    useEffect(() => {
+        socketRef.current = io(endPoint)
+
+        socketRef.current.on('message received', (message) => {
+            console.log(message)
+            setTexts((texts) => [...texts, message])
+            console.log(texts)
+        })
+    }, [])
+
     const updateMessages = (newText) => {
-        setTexts([...texts, newText])
-        console.log(texts)
+        socketRef.current.emit('message sent', newText)
     }
+    
     return (
         <Paper className={classes.messageContainer}>
             {texts.map(text => {
